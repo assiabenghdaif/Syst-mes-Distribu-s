404864df7cf19c4b836f22974a194ae7d8289eb7
Assia Benghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2023-05-29 00:17:14 +0100

    ok

---
+++ Distributed Systems Project/BackEnd/infraction-service/src/test/java/fsm/miaad/infractionservice/InfractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fsm.miaad.infractionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
