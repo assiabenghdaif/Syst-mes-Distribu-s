b5646795cba5c1f7fc7d899e03dde53521d4adae
Assia Benghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2023-05-29 14:43:14 +0100

    ok

---
+++ Distributed Systems Project/BackEnd/eureka-discovery/src/main/java/fsm/miaad/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package fsm.miaad.eurekadiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryApplication.class, args);
+    }
+
+}
