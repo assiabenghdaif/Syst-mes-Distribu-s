7a8e9b1bd22cb1b3040c7a7bfef9ebcb482a259b
assiabenghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2023-06-25 21:22:36 +0100

    TP6

---
+++ TP6/demo-springcloud/src/main/java/fsm/miaad/demospringcloud/DemoSpringcloudApplication.java
@@ -0,0 +1,14 @@
+package fsm.miaad.demospringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringcloudApplication.class, args);
+        System.out.println("----------------------");
+    }
+
+}
